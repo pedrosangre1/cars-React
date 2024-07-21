880224ec4c6e99b0c5e642f4c9a66a152fba7470
pedrosangre1 <sangre@abv.bg>
2024-07-22 00:15:20 +0300

    is work

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,12 @@
 import Header from "./componets/header/Header"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+   return (
   <div id="box">
- <h1>Cars</h1>
-<Header/>
-<main id="main-content">
-</main>
+     <h1>Cars</h1>
+      <Header/>
+    <main id="main-content">
+    </main>
  </div>
   )
 }
