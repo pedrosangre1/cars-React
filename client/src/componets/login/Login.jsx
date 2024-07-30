9424215eb8fd6da8d952e706e776b326ec5a5802
pedrosangre1 <sangre@abv.bg>
2024-07-30 03:35:05 +0300

    Login mod

--- client/src/componets/login/Login.jsx
+++ client/src/componets/login/Login.jsx
@@ -2,22 +2,27 @@ import { useNavigate } from "react-router-dom";
 import { useLogin } from "../../hooks/useAuth";
 import { useForm } from "../../hooks/useForm";
 
+const initialValues = {email: '', password: ''};
+
 export default function Login() {
 const login = useLogin();
 const navigate = useNavigate();
-const {values, changeHandler, submitHandler} = useForm(
-    {email: '', password: ''},
-   async ({ email, password }) =>{ 
+
+const loginHandler = async ({email, password}) =>{ 
     try{
         await login(email, password)
         navigate('/');
     }catch (err) {
         console.log(err.message);
     }
+};
 
-    },
-);
-
+const {
+    values,
+    changeHandler,
+    submitHandler,
+ } = useForm(initialValues,loginHandler);
+ 
 return (
 <section id="login-page" className="auth">
 <form id="login" onSubmit={submitHandler}>
