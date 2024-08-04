be008a90aea0371b7ffa92004d900ed09eb65c69
pedrosangre1 <sangre@abv.bg>
2024-08-04 23:15:18 +0300

    Add dinamic navigation

--- client/src/componets/header/Header.jsx
+++ client/src/componets/header/Header.jsx
@@ -1,19 +1,28 @@
+import { useContext } from 'react';
 import {Link} from 'react-router-dom'
+import { AuthContext } from '../../contexts/authContext';
 
 export default function Header() {
+    const {isAuthenticated} = useContext (AuthContext);
 return (
     <header>
     <h1><Link className="home" to="/">Коли</Link></h1>
     <nav>
-        <Link to="/games">Всички коли</Link>
-        <div id="user">
+        
+        {isAuthenticated
+        ?(<div id="user">
+            <Link to="/games">Всички коли</Link>
             <Link to="/games/create">Нова кола</Link>
             <Link to="/logaut">Изход</Link>
-        </div>
-        <div id="guest">
+        </div>)
+        :(
+            <div id="guest">
             <Link to="/login">Вход</Link>
             <Link to="/register">Регистрация</Link>
         </div>
+        )
+    }
+              
     </nav>
 </header>
 );
--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -7,9 +7,9 @@ export const useLogin = () => {
    const {changeAuthState} = useContext(AuthContext);
    
     const loginHandler = async (email, password) => {
-        const result = await login(email, password);
+        const {password: _, ...authData} = await login(email, password);
     
-     changeAuthState(result);
+     changeAuthState(authData);
 
         return result;
     }
@@ -20,9 +20,9 @@ export const useRegister = () => {
     const {changeAuthState} = useContext(AuthContext);
     
     const registerHandler = async (email, password) => {
-        const result = await register (email, password);
+        const {password: _, ...authData} = await register (email, password);
 
-        changeAuthState(result);
+        changeAuthState(authData);
 
         return result;
     }
