5a84e167db4c84f5078137bda9c1730f0788da66
pedrosangre1 <sangre@abv.bg>
2024-10-10 00:05:16 +0300

    its_alive

---
+++ client/src/componets/references/CarsRecords.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import GameList from '../game-list/GameList';
+
+
+
+export default function CarsRecord() {
+
+  
+  return (
+    
+    <div> 
+             <GameList/> 
+        {/*  <img className="game-img" src={car.photo} />
+          <h1>{car.marka}  {car.model}</h1>
+          <span className="levels">Регистрационен номер:{car.regno}</span>
+          <p className="type">Въведена на:{car.datetime}{formatDate(car._createdOn)}</p> */}
+    </div>
+  );
+}
